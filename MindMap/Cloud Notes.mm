7faafd071969df43f1aa9e4e8bb0af772315cd6e
Chris Williamson <chriswill0629@gmail.com>
2021-03-22 18:27:34 -0400

    Added mind map file

---
+++ MindMap/Cloud Notes.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Cloud Notes" FOLDED="false" ID="ID_1282520924" CREATED="1616385944532" MODIFIED="1616387356887" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Landing Page" FOLDED="true" POSITION="right" ID="ID_658080652" CREATED="1616386089126" MODIFIED="1616386122351" HGAP_QUANTITY="37.249999307096026 pt" VSHIFT_QUANTITY="35.249998949468164 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="" ID="ID_1931157021" CREATED="1616386103577" MODIFIED="1616386103577"/>
+</node>
+<node TEXT="About Cloud Notes" POSITION="left" ID="ID_1359569104" CREATED="1616386136333" MODIFIED="1616386213549" HGAP_QUANTITY="-281.499991193414 pt" VSHIFT_QUANTITY="86.99999740719804 pt">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Login" POSITION="left" ID="ID_1903876647" CREATED="1616386161120" MODIFIED="1616387350702" HGAP_QUANTITY="-219.2499930486085 pt" VSHIFT_QUANTITY="18.749999441206473 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="User Bar (TOP)" ID="ID_1737080505" CREATED="1616386426255" MODIFIED="1616386727217" HGAP_QUANTITY="-125.49999584257614 pt" VSHIFT_QUANTITY="6.74999979883433 pt">
+<node TEXT="Username (Profile)" ID="ID_505308900" CREATED="1616386609059" MODIFIED="1616386796030" HGAP_QUANTITY="-199.7499936297538 pt" VSHIFT_QUANTITY="17.249999485909953 pt">
+<node TEXT="Profile view allows for editing of name, avatar, and default note view." ID="ID_619132801" CREATED="1616386741627" MODIFIED="1616386796029" HGAP_QUANTITY="-408.24998741596977 pt" VSHIFT_QUANTITY="23.249999307096026 pt"/>
+</node>
+<node TEXT="Settings wheel" ID="ID_1586273139" CREATED="1616386684804" MODIFIED="1616386858785" HGAP_QUANTITY="-177.249994300306 pt" VSHIFT_QUANTITY="4.499999865889553 pt">
+<node TEXT="Left Pane" ID="ID_1473866323" CREATED="1616386813694" MODIFIED="1616386895085" HGAP_QUANTITY="-158.49999485909953 pt" VSHIFT_QUANTITY="19.49999941885473 pt">
+<node TEXT="General" ID="ID_1670682861" CREATED="1616386862271" MODIFIED="1616387109514" HGAP_QUANTITY="-123.99999588727962 pt" VSHIFT_QUANTITY="24.749999262392542 pt">
+<node TEXT="Apperance Preferences" ID="ID_999641304" CREATED="1616386956717" MODIFIED="1616386975157" HGAP_QUANTITY="-187.74999398738163 pt" VSHIFT_QUANTITY="17.249999485909953 pt"/>
+<node TEXT="Sync Preferences" ID="ID_1940719251" CREATED="1616386977554" MODIFIED="1616387029901" HGAP_QUANTITY="-160.7499947920443 pt" VSHIFT_QUANTITY="7.499999776482589 pt"/>
+<node TEXT="Other" ID="ID_1123649584" CREATED="1616387037192" MODIFIED="1616387109513" HGAP_QUANTITY="-108.99999633431446 pt" VSHIFT_QUANTITY="3.7499998882412946 pt"/>
+</node>
+<node TEXT="Sub Accounts" ID="ID_1731348883" CREATED="1616386875457" MODIFIED="1616387226833" HGAP_QUANTITY="-150.99999508261695 pt" VSHIFT_QUANTITY="11.249999664723884 pt">
+<node TEXT="Sub account view shows accounts accounts created within main account. Sub accounts are directly linked to the mian account and see all notes created between main and sub account. Notes will show time stamp and username of person who created and also who last edited." ID="ID_8946159" CREATED="1616387111495" MODIFIED="1616387226830" HGAP_QUANTITY="-370.7499885335568 pt" VSHIFT_QUANTITY="32.24999903887513 pt"/>
+</node>
+<node TEXT="Linked Accounts" ID="ID_850113453" CREATED="1616386885720" MODIFIED="1616387316726" HGAP_QUANTITY="-159.2499948367478 pt" VSHIFT_QUANTITY="12.7499996200204 pt">
+<node TEXT="Linked accounts view shows accounts that are linked for sharing of notes. These are separate main accounts with their own note views but have the ability to share notes seamlessly with one another. Notes will show time stamp and username of person who created and also who last edited." ID="ID_1146426882" CREATED="1616387233750" MODIFIED="1616387316722" HGAP_QUANTITY="-381.2499882206324 pt" VSHIFT_QUANTITY="33.74999899417165 pt"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Left Pane" ID="ID_1704032127" CREATED="1616386294422" MODIFIED="1616387346510" HGAP_QUANTITY="-116.49999611079704 pt" VSHIFT_QUANTITY="-2.9999999105930364 pt">
+<node TEXT="Category / Folder View" ID="ID_1790252344" CREATED="1616386320734" MODIFIED="1616386352130" HGAP_QUANTITY="-177.249994300306 pt" VSHIFT_QUANTITY="17.99999946355821 pt"/>
+</node>
+<node TEXT="right Pane" ID="ID_307252819" CREATED="1616386373408" MODIFIED="1616387350702" HGAP_QUANTITY="-104.49999646842491 pt" VSHIFT_QUANTITY="36.74999890476468 pt">
+<node TEXT="Note View" ID="ID_1333246949" CREATED="1616386404818" MODIFIED="1616387462723" HGAP_QUANTITY="-120.99999597668659 pt" VSHIFT_QUANTITY="22.499999329447768 pt">
+<node TEXT="Number Notes" ID="ID_608436219" CREATED="1616387387437" MODIFIED="1616387399493" HGAP_QUANTITY="-153.99999499320998 pt" VSHIFT_QUANTITY="21.749999351799506 pt"/>
+<node TEXT="Check Mark Notes" ID="ID_97876430" CREATED="1616387403838" MODIFIED="1616387457805" HGAP_QUANTITY="-169.74999452382343 pt" VSHIFT_QUANTITY="3.749999888241291 pt"/>
+<node TEXT="Plain Notes" ID="ID_983713735" CREATED="1616387438518" MODIFIED="1616387462723" HGAP_QUANTITY="-134.49999557435527 pt" VSHIFT_QUANTITY="3.7499998882412946 pt"/>
+</node>
+</node>
+</node>
+<node TEXT="Sign Up" POSITION="left" ID="ID_697528125" CREATED="1616386193944" MODIFIED="1616387356887" HGAP_QUANTITY="-274.7499913945797 pt" VSHIFT_QUANTITY="35.99999892711642 pt">
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
